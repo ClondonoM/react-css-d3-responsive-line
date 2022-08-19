b5b64066db54ead11657b29bb346c852b3ab76ce
Carlos Londono <c.londono.m@gmail.com>
2022-08-18 19:03:49 -0500

    configuring initial data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import initialData from './constants/initialData';
+
 function App() {
+  const width = 500;
+  const height = 150;
+  const padding = 20;
+  const maxValue = 20;
+
   return (
     <div className='App'>
       <p>D3 responsive line</p>
