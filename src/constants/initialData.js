4904ef33255ea9d1fafea5510716b4fbb4ab50a4
Carlos Londono <c.londono.m@gmail.com>
2022-08-18 19:04:08 -0500

    configuring initial data

---
+++ src/constants/initialData.js
@@ -0,0 +1,22 @@
+export const initialData = [
+  {
+    name: 'Car',
+    value: 10,
+  },
+  {
+    name: 'Food',
+    value: 3,
+  },
+  {
+    name: 'Telephone',
+    value: 9,
+  },
+  {
+    name: 'Electricity',
+    value: 7,
+  },
+  {
+    name: 'Cinema',
+    value: 7,
+  },
+];
